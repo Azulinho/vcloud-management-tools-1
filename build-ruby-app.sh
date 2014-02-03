7c353f4cfe725342101d08b76fdd76121976c97f
Azul <mail@azulinho.com>
2014-02-03 14:49:10 +0000

    adding build-ruby-app draft script

---
+++ build-ruby-app.sh
@@ -0,0 +1,46 @@
+#!/bin/bas
+
+
+RUBY_VERSION=2.1.0
+OPT_INSTALL_PATH=/opt/DVLA/vcloud-management-tools
+APP_NAME="vcloud-create-vapp"
+
+#disinfect work areas
+TIMESTAMP=$( date +%s)
+BUILD_DIR=$PWD/build
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+
+INSTALL_DIR=$PWD/install
+rm -rf $INSTALL_DIR
+mkdir $INSTALL_DIR
+
+OUTPUT_DIR=$PWD/output
+rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
+
+#BUILD
+#Fix environment
+DEPENDS="ruby-local-$RUBY_VERSION"
+export PATH=/opt/$DEPENDS/bin:$PATH
+
+git clone https://github.com/dvla/vcloud-management-tools.git $INSTALL_DIR/$OPT_INSTALL_PATH
+
+cd $INSTALL_DIR/$OPT_INSTALL_PATH
+rm -rf .git #remove git files
+bundle install --deployment --binstubs
+bundle package --all
+
+#make symlink into ./usr/bin
+rm -f $INSTALL_DIR/usr/bin/$APP_NAME
+mkdir -p $INSTALL_DIR/usr/bin/
+cd $INSTALL_DIR
+ln -s $OPT_INSTALL_PATH/bin/$APP_NAME usr/bin/$APP_NAME
+cd -
+
+#make rpm package
+fpm -s dir -t rpm -n $APP_NAME -v 1.0.$TIMESTAMP -d $DEPENDS -C $INSTALL_DIR -p $OUTPUT_DIR .
+
+#tidy up
+rm -rf $INSTALL_DIR
+rm -rf $BUILD_DIR
