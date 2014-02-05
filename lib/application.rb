f741f8af0bd90885d44da5000d08f73281e3c6bc
Azul <mail@azulinho.com>
2014-02-05 14:46:47 +0000

    reconfigured to use DHCP

--- lib/application.rb
+++ lib/application.rb
@@ -52,9 +52,11 @@ vcloud = VCloudClient::Connection.new(vcloud_credentials['host'],
 vcloud.login
 
 # retrieve the list of vcloud organizations this user has access
+sleep 5
 orgs = vcloud.get_organizations
 
 # retrieve all the objects within this specific organization
+sleep 5
 org = vcloud.get_organization(orgs[vcloud_credentials['org_name']])
 
 # retrieve the list of networks within the organization
@@ -65,6 +67,7 @@ vdcs = org[:vdcs]
 ap vdcs
 
 # retrieve catalog uuid and then the list of all vapps inside that catalog
+sleep 5
 catalog_uuid = vcloud.get_catalog_id_by_name(org,
                                              vcloud_credentials['catalog_name'])
 
@@ -75,12 +78,14 @@ catalog_uuid = vcloud.get_catalog_id_by_name(org,
 # the item object in the catalog contains a set of children items inside it
 # and we require the first :id contained in it.
 #
+sleep 5
 vapp_template_uuid = vcloud.get_catalog_item_by_name(
   catalog_uuid,
   cmdline_args[:vapp_template],
   )[:items][0][:id]
 
 # create a new VAPP called 'vappname1'
+sleep 5
 vapp = vcloud.create_vapp_from_template(
   vcloud_credentials['vdc_name'],
   cmdline_args[:vapp_name],
@@ -93,7 +98,7 @@ ap vapp
 # wait until the VAPP is deployed
 #vapp_info = vcloud.get_vapp(vapp[:vapp_id])
 #ap vapp_info
-
+sleep 5
 vcloud.wait_task_completion(vapp[:task_id])
 
 config =  {
@@ -101,19 +106,33 @@ config =  {
   :fence_mode => "bridged",
   :parent_network =>  {
     :id => networks[cmdline_args[:org_network]] },
-  :ip_allocation_mode => "POOL" }
+  :ip_allocation_mode => "DHCP" }
 
 network_uuid = networks[cmdline_args[:org_network]]
 
+sleep 5
 network = vcloud.get_network(network_uuid)
 
 #reconfigure the networks for the vapp
+sleep 5
 vcloud.add_org_network_to_vapp(vapp[:vapp_id], network, config)
 
-#find out the ip address of my new vapp
-ip_address = vcloud.get_vapp(vapp[:vapp_id])[:ip]
-ap ip_address
 
+# poweron the vapp
+sleep 5
+poweron_taskid = vcloud.poweron_vapp(vapp[:vapp_id])
+ap poweron_taskid
+sleep 5
+vcloud.wait_task_completion(poweron_taskid)
+
+# find out the ip address of my new vapp
+# this can take a while, so lets loop around it for a bit
+sleep 5
+until ip_address = vcloud.get_vapp(vapp[:vapp_id])[:ip] do
+  puts "waiting for ip address information..."
+  sleep 15
+end
+ap ip_address
 
 # register new vapp into DNS
 dnsimple = Fog::DNS.new({
@@ -122,9 +141,11 @@ dnsimple = Fog::DNS.new({
   :dnsimple_password => dnsimple_credentials['password']
 })
 
+sleep 1
 zone = dnsimple.zones.get(cmdline_args[:domain])
 ap zone
 
+sleep 1
 record = zone.records.create(
 :value => ip_address,
 :name => cmdline_args[:vapp_name] + '.' + cmdline_args[:subdomain],
